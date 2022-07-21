9eb46f6ccd91ee2bbc29b854dd731a2e05d80e32
teeflow1 <105053064+teeflow1@users.noreply.github.com>
2022-07-21 04:59:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,123 @@
+class Calculator{
+    constructor{previousOperandTextElement, currentOperandTextElement}{
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+    clear(){
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    delete(){
+        this.currentOperand = this.currentOperand.testString().slice(0, -1)
+    }
+    appendNumber(number){
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() 
+        + number. toString()
+    }
+
+    chooseOperation(operation){
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== ''){
+            this.compute()
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand ='';
+    }
+    compute(){
+        let computation 
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if(isNaN(prev) || isNaN(current)) return;
+        switch(this.operation) {
+            
+            case '+':
+            computation = prev + current;
+            break; 
+            case '-':
+            computation = prev - current;
+            break
+            case *:
+                computation = prev * current;
+            break
+            case '/':
+                computation= prev/current;
+                break;
+
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+    getDsiplayNumber(number){
+        const stringNumber = number.testString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN (integerDigits)){
+            integerDisplay = ''
+        }else{
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits:0})
+        }
+        if(decimalDigits != null){
+            return +${integerDisplay}. ${decimalDigits}
+        } else{
+            return integerDisplay
+        }
+        
+
+    }
+    updateDisplay(){
+        this.currentOperandTextElement.InnerText =  this.getDsiplayNumber(this.currentOperand)
+        if(this.operation != null){
+            this.previousOperandTextElement.InnerText = 
+            + ${this.getDsiplayNumber(this.previousOperand)} $(this.operation)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equal]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+
+const Calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button  =>{
+    button.addEventListener('click', () =>{
+        calculator.chooseOperation(button.InnerText)
+        calculator.updateDisplay()
+    })
+})
+
+
+equalsButton.addEventListener('click',  button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click',  button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click',  button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
